3c70e216c45f5031f8a3e45441f774cf2255a85a
Hai Vu <haivu2004@gmail.com>
2025-02-23 20:10:55 -0800

    Reoganize tests using parametrize

--- src/test/test_tree.py
+++ src/test/test_tree.py
@@ -1,48 +1,29 @@
 import pytest
-from click.testing import CliRunner, Result
+from click.testing import CliRunner
 
 from nginx_conf_tool.tree import tree
 
 
-@pytest.fixture(scope="module")
-def runner():
-    return CliRunner()
+@pytest.mark.parametrize(
+    ["args", "expected_code", "expected_output_file"],
+    [
+        pytest.param([], 2, "", id="missing file"),
+        pytest.param(["samples/simple.conf"], 0, "simple_output.txt", id="no option"),
+        pytest.param(
+            ["-d", "samples/simple.conf"], 0, "simple_dir_only.txt", id="-d option"
+        ),
+        pytest.param(
+            ["-L", "1", "samples/simple.conf"], 0, "simple_level1.txt", id="-L option"
+        ),
+    ],
+)
+def test_simple_verify_stdout(request, args, expected_code, expected_output_file):
+    runner = CliRunner()
+    result = runner.invoke(tree, args=args)
 
+    assert result.exit_code == expected_code
 
-@pytest.fixture(scope="module")
-def simple_result(runner) -> Result:
-    return runner.invoke(tree, ["samples/simple.conf"])
-
-
-@pytest.fixture(scope="module")
-def data_dir(request):
-    print(request.path)
-    return request.path.parent / "data"
-
-
-def test_missing_file(runner):
-    result = runner.invoke(tree)
-    assert result.exit_code != 0
-
-
-def test_simple_verify_exit_code(simple_result):
-    assert simple_result.exit_code == 0
-
-
-def test_simple_verify_stdout(simple_result, data_dir):
-    expected = (data_dir / "simple_output.txt").read_text()
-    assert simple_result.output == expected
-
-
-def test_simple_dir_only(runner, data_dir):
-    expected = (data_dir / "simple_dir_only.txt").read_text()
-    result = runner.invoke(tree, ["-d", "samples/simple.conf"])
-    assert result.exit_code == 0
-    assert result.output == expected
-
-
-def test_simple_level1(runner, data_dir):
-    expected = (data_dir / "simple_level1.txt").read_text()
-    result = runner.invoke(tree, ["-L", "1", "samples/simple.conf"])
-    assert result.exit_code == 0
-    assert result.output == expected
+    if expected_code == 0:
+        data_dir = request.path.parent / "data"
+        expected = (data_dir / expected_output_file).read_text()
+        assert result.output == expected
